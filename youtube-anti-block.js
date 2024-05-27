1de99cc5ad488744e0aba81d1b1f4fc3d89c74a6
Kaio HSG <96930584+KaioHSG@users.noreply.github.com>
2024-05-27 00:48:52 -0300

    Create youtube-anti-block.js

---
+++ youtube-anti-block.js
@@ -0,0 +1,8 @@
+[Adblock Plus 2.0]
+! Title: YouTube Anti-Block
+! Version: 1
+! Homepage: https://github.com/KaioHSG/youtube-anti-block/
+youtube.com##+js(set, yt.config_.openPopupConfig.supportedPopups.adBlockMessageViewModel, false)
+youtube.com##+js(set, Object.prototype.adBlocksFound, 0)
+youtube.com##+js(set, ytplayer.config.args.raw_player_response.adPlacements, [])
+youtube.com##+js(set, Object.prototype.hasAllowedInstreamAd, true)
